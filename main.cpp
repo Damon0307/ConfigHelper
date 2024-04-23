c20c5c7060d6ef0c9fa105611a562d0363757da4
Damon0307 <1294056177@qq.com>
2024-04-23 21:01:25 +0800

    Add files via upload
    Hello ConfigHelper

---
+++ main.cpp
@@ -0,0 +1,20 @@
+
+#include <string>
+#include <iostream>
+#include "elog/logger.h"
+ 
+using namespace std;
+using namespace elog;
+
+int main()
+{
+    Log::trace("hello elog4cpp");
+    Log::debug("hello elog4cpp");
+    Log::info("hello elog4cpp");
+    Log::warn("hello elog4cpp");
+    Log::error("hello elog4cpp");
+    //Log::fatal("hello elog4cpp");
+
+    std::cout << "/* message */" << std::endl;
+    
+}
